63c588ccfc01eef8dbfb7744548f7c5e29c2220a
Jackson Barreto <jackson.barreto@gmail.com>
2022-12-16 12:04:04 +0000

    Create test_environment.py

---
+++ test_environment.py
@@ -0,0 +1,13 @@
+from dnssec.DNSSecInspector import DNSSecInspector
+from httpsec.HTTPSInspector import HTTPSInspector
+
+if __name__ == '__main__':
+
+    # url = 'www.cm-agueda.pt'
+
+    url = 'www.cm-albergaria.pt'
+
+
+    for x in range(100):
+        print(HTTPSInspector(url).inspect().get_information())
+        print(DNSSecInspector(url).inspect().get_information())
